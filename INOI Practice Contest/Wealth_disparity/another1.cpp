85793d330767fd94f81f1915d9d7ade567c40260
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-07-21 00:32:18 +0530

    INOI1601

--- INOI Practice Contest/Wealth_disparity/another1.cpp
+++ INOI Practice Contest/Wealth_disparity/another1.cpp
@@ -7,12 +7,6 @@ typedef long long int lld;
 
 using namespace std;
 
-// class Datatype{
-//     public:
-//     vector<int*>children_addres;
-//     int value;
-// };
-
 lld max1(lld i,lld j)
 {
     if (i>j)
@@ -35,7 +29,6 @@ int main(int argc, char const *argv[])
     // tree = new vector<lld>[n];
     vector<vector<int>> tree(n+1);
 
-
     for (int i = 0; i < n; i++)
     {
         cin>>values[i];
@@ -53,15 +46,12 @@ int main(int argc, char const *argv[])
         }
         else
         {
-            //every accept parent as -1
             tree[parentlist1[i]-1].push_back(i); //index taken as 0
         }
         
         //tree
     }
 
-    // problem is to maximise from children and parent 
-    // therefore current parent if children are maximum then parent has children score 
     bool *visted = new bool[n];
    for (lld ij = 0; ij < n; ij++)
     {
@@ -79,64 +69,28 @@ int main(int argc, char const *argv[])
     queue<int> q;
     q.push(root_idx);
     long noedmain,noedchild;
-    
 
-    
     while (!q.empty())
     {
         noedmain = q.front();
-        q.pop();// done with this shit
-        // visted[noedmain] = true; // visited this shit
-        //go through all children of nodes
+        q.pop();
         for (unsigned int i12 = 0; i12 < tree[noedmain].size(); i12++)
-        {//going through children indexex
-        //selecting children
+        {//going through childre
+ 
             noedchild = tree[noedmain][i12];
             if (!visted[noedchild])
             {
-                q.push(noedchild);//add node to see its children
+                q.push(noedchild);
                 visted[noedchild] = true;//done
                 finalanswer = max1(finalanswer,(new_updatedvalueparent[noedmain]-values[noedchild]));
                 new_updatedvalueparent[noedchild] = max(values[noedchild],new_updatedvalueparent[noedmain]) ;//if individual value is smaller than parent than copy the parent value
 
-
-
             }
             
-
         }
         
-
     }
     cout<<finalanswer;    
-
-
-    
-
-
-
-//tree check
-
-    // for (unsigned int i1j = 0; i1j < n; i1j++)
-    // {
-    //     cout<<endl<<i1j<<endl;
-    //     for (unsigned int i = 0; i < tree[i1j].size(); i++)
-    //     {
-    //         cout<<tree[i1j][i]<<" ";
-
-    //     }
-        
-    // }
-    
-
-
-
-
-
-
-
-
-
    delete [] parentlist1;delete [] values;
    delete [] visted;delete [] new_updatedvalueparent;
 //    delete [] tree;
