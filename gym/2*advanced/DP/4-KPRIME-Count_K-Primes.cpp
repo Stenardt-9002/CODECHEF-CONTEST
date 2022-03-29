e6561df813bdb62e2e4d3e09d95f664362835fe8
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2022-03-29 23:54:08 +0530

    DP 2* advanced practice

---
+++ gym/2*advanced/DP/4-KPRIME-Count_K-Primes.cpp
@@ -0,0 +1,60 @@
+
+// https://www.codechef.com/problems/KPRIME
+// https://discuss.codechef.com/t/kprime-editorial/2651
+
+#include <bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+
+using namespace std;
+using namespace __gnu_pbds ;
+
+typedef  long long ll1d;
+using namespace std;
+
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+     	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+
+
+    ll1d testcase ,n,i1,j1;
+  cin>>testcase ;
+    ll1d a1,b1,k;
+    while (testcase--)
+    {
+        cin>>a1>>b1>>k ;
+        vector<vector<int>> dp1 ;
+        for ( i1 = 0; i1 < 5; i1++)
+        {
+            vector<int> temp1(1e5,0);
+            dp1.push_back(temp1);
+        }
+        
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
