94f968abc9f088f86be96c3c38eccf4ebe5c5b8b
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2022-04-21 23:57:51 +0530

    DP 2* advanced 2

--- gym/2*advanced/DP/4-KPRIME-Count_K-Primes.cpp
+++ gym/2*advanced/DP/4-KPRIME-Count_K-Primes.cpp
@@ -25,19 +25,97 @@ int main(int argc, char const *argv[])
 	cout.tie(0);
 
 
+    vector<int> primes_list ; // prim enumbers
+    vector<int> unique_fac(100000,0); //start from 2 
+    primes_list.push_back(2);
+    primes_list.push_back(3);
+    primes_list.push_back(5);
 
+    unique_fac[0] = 1;//1
+    // unique_fac[1] = 1;//2
+    // unique_fac[2] = 1;//3
+    // unique_fac[3] = 1;//4
+    // unique_fac[4] = 1;//5
+
+
+    // for (int i1 = 7; i1 < 100; i1++)
+    for (int i1 = 7; i1 < 100000; i1++)
+    {
+        int till = (int)(sqrt(i1))+1 ;
+        bool prime_check = false ;
+        for (int j1 = 0; primes_list[j1] < till; j1++)
+        {
+            if((i1%primes_list[j1])==0)
+            {
+                prime_check = true; 
+                break ;
+            }
+        }
+        if(!prime_check)
+        {
+            primes_list.push_back(i1);
+            // unique_fac[i1-1]=1;
+        }
+    }
+
+    // for (int i1 = 6; i1 < 100005; i1++)
+    // for (int i1 = 3; i1 < 100005; i1++)
+    for (int i1 = 3; i1 < primes_list.size(); i1++)
+    {
+        int multiple=2 ;
+        int number = i1-1;
+        while(number*multiple<100001)
+        {
+            unique_fac[number]++;
+            primes_list[i1-1];
+            multiple++ ;
+        }
+
+
+            // // int till = (int)(sqrt(i1))+1 ;
+            // int till = (int)(i1/2) ;
+            // // for (int j1 = 0; primes_list[j1] < till+1; j1++)
+            // for (int j1 = 0; primes_list[j1] < till+1; j1++)
+            // {
+            //     // if(i1%primes_list[j1]==0  && (i1/primes_list[j1])<till+1)
+            //     //     unique_fac[i1-1]++;
+            //     // else if(i1%primes_list[j1]==0 )
+            //     //     unique_fac[i1-1]+=2;
+            //     if(i1%primes_list[j1]==0 )
+            //         unique_fac[i1-1]++;
+            // }
+    }
+    
+
+
+
+    // cout<<endl;
+    // // for (auto x : primes_list)
+    // for (int i1=0 ;i1<100; i1++)
+    //     cout<<" "<<primes_list[i1];
+
+    // cout<<endl;
+    // for (int i1=1 ;i1<100; i1++)
+    //     cout<<i1<<" "<<unique_fac[i1-1]<<"     ";
+    
     ll1d testcase ,n,i1,j1;
-  cin>>testcase ;
+    cin>>testcase ;
     ll1d a1,b1,k;
     while (testcase--)
     {
+        int cunt1 = 0;
         cin>>a1>>b1>>k ;
-        vector<vector<int>> dp1 ;
-        for ( i1 = 0; i1 < 5; i1++)
-        {
-            vector<int> temp1(1e5,0);
-            dp1.push_back(temp1);
-        }
+        for (i1 = a1; i1 <=b1; i1++)
+            if(unique_fac[i1-1]==k)
+                cunt1++ ;
+        
+        cout<<cunt1<<endl;
+        // vector<vector<int>> dp1 ;
+        // for ( i1 = 0; i1 < 5; i1++)
+        // {
+        //     vector<int> temp1(1e5,0);
+        //     dp1.push_back(temp1);
+        // }
         
     }
     return 0;
